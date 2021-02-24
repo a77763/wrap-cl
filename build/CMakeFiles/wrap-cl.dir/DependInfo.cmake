f999a3bcb9926dfaeceb7f72989534850e138a06
Luís Neto <lmpn@MacBook-Pro-de-Luis.local>
2021-02-24 13:40:31 +0100

    added cmake; removed main

---
+++ build/CMakeFiles/wrap-cl.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/lmpn/Desktop/conan_example/wrap-cl/src/clcore.cpp" "/Users/lmpn/Desktop/conan_example/wrap-cl/build/CMakeFiles/wrap-cl.dir/src/clcore.cpp.o"
+  "/Users/lmpn/Desktop/conan_example/wrap-cl/src/clkernel.cpp" "/Users/lmpn/Desktop/conan_example/wrap-cl/build/CMakeFiles/wrap-cl.dir/src/clkernel.cpp.o"
+  "/Users/lmpn/Desktop/conan_example/wrap-cl/src/clmemory.cpp" "/Users/lmpn/Desktop/conan_example/wrap-cl/build/CMakeFiles/wrap-cl.dir/src/clmemory.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk/System/Library/Frameworks/OpenCL.framework"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/wrap-cl.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wrap-cl.dir/src/clcore.cpp.o"
+  "CMakeFiles/wrap-cl.dir/src/clkernel.cpp.o"
+  "CMakeFiles/wrap-cl.dir/src/clmemory.cpp.o"
+  "libwrap-cl.a"
+  "libwrap-cl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wrap-cl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
